f7fa9a41d854703147650b9f6e0d796691283e63
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-12-14 00:08:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Product name="laptop" price="10000"></Product>
+      <Product name="mobile" price="20000"></Product>
+      <Product name="Watch" price="30000"></Product>
+      <Product name="headphone" price="40000"></Product> */}
+      {/* <Count></Count> */}
+      <Users></Users>
     </div>
   );
+}
+
+// function Product(props) {
+//   return (
+//     <div className='product'>
+//       <h3>Name : {props.name}</h3>
+//       <p>Price : {props.price}</p>
+//     </div>
+//   );
+// }
+
+
+
+// function Count() {
+//   const [count, setCount] = useState(0);
+//   const increase = () => {
+//     setCount(count + 1)
+//   }
+//   const decrease = () => {
+//     if (count > 0) {
+//       setCount(count - 1)
+//     }
+//   }
+//   return (
+//     <div>
+//       <h3>Count : {count}</h3>
+//       <button onClick={increase}>increase</button>
+//       <button onClick={decrease}>decrease</button>
+//     </div>
+//   );
+// }
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>External User</h2>
+      {
+        users.map(user => <Person name={user.name} key={user.id}></Person>)
+      }
+    </div>
+  )
+}
+
+function Person(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>userName : {props.name}</h1>
+    </div>
+  )
 }
 
 export default App;
